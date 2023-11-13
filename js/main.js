07c872dbc3a4a5f167cd7eb6b0c7ae3f624ed028
MatteoFarinazzo <matteofary@gmail.com>
2023-11-13 15:33:24 +0100

    half work

---
+++ js/main.js
@@ -0,0 +1,62 @@
+// variabili
+
+let name;
+let surname;
+let age;
+let rangeKilometers;
+
+
+//chiedere dati all'utente:nome, coignome, età, chilometri da percorrere
+
+
+function user_data(){
+name = prompt("Inserisci il tuo nome");
+surname = prompt("Inserisci il tuo cognome");
+age = prompt("Inserisci la tua età");
+if (isNaN(age)) {
+    alert("Inserisci un numero per indicare l'età!");
+    return; 
+}
+rangeKilometers = prompt("Inserisci quanti chilometri vuoi percorrere");
+if (isNaN(rangeKilometers))
+    alert("Inserisci un numero per indicare i chilometri!");
+    return;
+
+// convertire in numeri
+
+//calcolare il prezzo del viaggio (numero di chilometri * 0.21£)
+}
+function base_price(){
+console.log("Il prezzo del tuo biglietto è:" + rangeKilometers * 0.21);
+let price = (rangeKilometers * 0.21);
+
+document.getElementById("name").innerHTML = name;
+document.getElementById("surname").innerHTML = surname;
+document.getElementById("distance").innerHTML = rangeKilometers;
+document.getElementById("price").innerHTML = price;
+
+}
+
+
+//calcolare eventuale sconto da applicare al prezzo calcolato in precedenza
+function discounte_price(){
+if (age<18)
+    console.log("Il prezzo del tuo biglietto è:" + (rangeKilometers *0.21) / 100 * 80 );
+
+
+else if (age>=65)
+    console.log("Il prezzo del tuo biglietto è:" + (rangeKilometers *0.21) / 100 * 60 );
+
+else
+    console.log("Non hai sconti da applicare");
+
+    document.getElementById("name").innerHTML = name;
+
+}
+
+
+
+
+
+//output sulla pagina html con un massimo di due decimali (centesimi) per indicare il prezzo
+
